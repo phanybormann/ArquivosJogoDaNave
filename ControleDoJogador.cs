d55621fd434a6bc7b38f0c22edb33fe0042e76cf
Stephany <stephanybormann13@gmail.com>
2023-12-02 12:53:27 +0000

    adicionando arquivos iniciais

---
+++ ControleDoJogador.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDoJogador : MonoBehaviour
+{
+    public Rigidbody2D oRigidbody2D;
+    public GameObject laserDoJogador;
+    public Transform localDoDisparoUnico;
+    public float velocidadeDaNave;
+
+    public bool temLaserDuplo;
+    
+    private Vector2 teclasApertadas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temLaserDuplo = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarJogador();
+        AtirarLaser();
+    }
+
+    private void MovimentarJogador()
+    {
+        teclasApertadas = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        oRigidbody2D.velocity = teclasApertadas.normalized * velocidadeDaNave;
+      }
+
+    private void AtirarLaser()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(temLaserDuplo == false)
+            {
+                Instantiate(laserDoJogador, localDoDisparoUnico.position, localDoDisparoUnico.rotation);
+            }
+        }
+    }
+
+}
---
+++ VidaDoJogador.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaDoJogador : MonoBehaviour
+{
+    public Slider barraDeVidaDoJogador;
+    public int vidaMaximaDoJogador;
+    public int vidaAtualDoJogador;
+    public bool temEscudo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaAtualDoJogador = vidaMaximaDoJogador;
+        barraDeVidaDoJogador.maxValue = vidaMaximaDoJogador;
+        barraDeVidaDoJogador.value = vidaAtualDoJogador;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MachucarJogador(int danoParaReceber)
+    {
+        if(temEscudo == false)
+        {
+            vidaAtualDoJogador -= danoParaReceber;
+            barraDeVidaDoJogador.value = vidaAtualDoJogador; 
+
+            if(vidaAtualDoJogador <= 0)
+            {
+                Debug.Log("Game Over");
+            }
+        }
+    }
+}
